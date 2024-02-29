271fe7d84a816fb63a93080d74fcbdbcdb38d4f1
Tadiwanashe Matthew Maenzanise <155242818+mattlikescode@users.noreply.github.com>
2024-02-29 12:53:48 +0100

    Second commit

--- src/HelloVistula.java
+++ src/HelloVistula.java
@@ -1,5 +1,6 @@
 public class HelloVistula {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula");
+        System.out.println("Hello Vistula!");
+        System.out.println("This is the first task");
     }
 }
